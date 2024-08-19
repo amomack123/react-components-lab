9953ed4244e81b6bd37a1a90a4236232053d5314
Amogh <amogh.maheshwari@email.com>
2024-08-19 03:11:22 -0700

    Finished project

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,13 @@
+import WeatherIcon from './WeatherIcon';
+import WeatherData from './WeatherData';
+
+const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
+  return (
+    <div className="weather">
+      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherIcon img={img} imgAlt={imgAlt} />
+    </div>
+  );
+}
+
+export default WeatherForecast;
